f78a25edc5ea09bdbe2b09ec56d56ed8bf598726
Sethijai <148748128+Sethijai@users.noreply.github.com>
2025-09-23 09:29:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/Jisshubot/Jisshu-forward-bot Jisshubot/Jisshu-forward-bot 
+  git clone https://github.com/Sethijai/Forwards Sethijai/Forwards 
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/Jisshubot/Jisshu-forward-bot -b $BRANCH /Jisshu-forward-bot
+  git clone https://github.com/Sethijai/Forwards -b $BRANCH /Forwards
 fi
-cd Jisshubot/Jisshu-forward-bot 
+cd Sethijai/Forwards 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 gunicorn app:app & python3 main.py
