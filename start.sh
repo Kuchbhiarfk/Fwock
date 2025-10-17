6a07d99d8ec289a6f6548f0076c21f2df5480174
Kuchbhiarfk <cha.r.dl.o.o62@googlemail.com>
2025-10-17 21:59:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/Sethijai/Forwards Sethijai/Forwards 
+  git clone https://github.com/Kuchbhiarfk/Fwock Kuchbhiarfk/Fwock 
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/Sethijai/Forwards -b $BRANCH /Forwards
+  git clone https://github.com/Kuchbhiarfk/Fwock -b $BRANCH /Fwock
 fi
-cd Sethijai/Forwards 
+cd Kuchbhiarfk/Fwock 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 gunicorn app:app & python3 main.py
